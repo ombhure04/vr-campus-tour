4c77cbbfc654b267c55611be2548d324b7ac9d19
Om Bhure <bhure.om04@gmail.com>
2026-04-26 19:38:12 +0530

    fix aframe types + VR update

--- src/types/aframe.d.ts
+++ src/types/aframe.d.ts
@@ -1,19 +1,21 @@
 declare namespace JSX {
   interface IntrinsicElements {
-    'a-scene': any;
-    'a-entity': any;
-    'a-camera': any;
-    'a-cursor': any;
-    'a-sky': any;
-    'a-circle': any;
-    'a-triangle': any;
-    'a-sphere': any;
-    'a-cone': any;
-    'a-text': any;
-    'a-plane': any;
-    'a-box': any;
-    'a-cylinder': any;
-    'a-assets': any;
-    'a-image': any;
+    "a-scene": any;
+    "a-entity": any;
+    "a-camera": any;
+    "a-cursor": any;
+    "a-sky": any;
+    "a-circle": any;
+    "a-triangle": any;
+    "a-sphere": any;
+    "a-cone": any;
+    "a-text": any;
+    "a-plane": any;
+    "a-box": any;
+    "a-cylinder": any;
+    "a-assets": any;
+    "a-image": any;
   }
 }
+
+declare module "aframe";
